changed wrong calculation of logvar

code/vae_lib/utils/distributions.py
@@ -12,11 +12,10 @@ if torch.cuda.is_available():
     PI = PI.cuda()
 
 # N(x | mu, var) = 1/sqrt{2pi var} exp[-1/(2 var) (x-mean)(x-mean)]
-# log N(x| mu, var) = -log sqrt(2pi) -0.5 log var - 0.5 (x-mean)(x-mean)/var
-
+# log N(x| mu, var) = -log sqrt(2pi) -0.5 log var - 0.5 (x-mean)(x-mean)/var ??? why 0.5 log var ???
 
 def log_normal_diag(x, mean, log_var, average=False, reduce=True, dim=None):
-    log_norm = -0.5 * (log_var + (x - mean) * (x - mean) * log_var.exp().reciprocal())
+    log_norm = -0.5 * ((x - mean) * (x - mean) * log_var.exp().reciprocal())
     if reduce:
         if average:
             return torch.mean(log_norm, dim)